e3621641b5824a90b987b4950bd17f25c6fdba29
Faruque Sarker <fsarker@amazon.co.uk>
2022-08-26 18:02:08 +0100

    fixed env var for lambda

--- lambda/rgta-sync/lambda_function.py
+++ lambda/rgta-sync/lambda_function.py
@@ -21,7 +21,7 @@ COST_REPORT_DDB_TABLE_NAME = os.environ.get("COST_REPORT_DDB_TABLE_NAME")
 
 def get_resource_regions():
     resource_regions = []
-    raw_resource_regions = os.environ.get('RESOURCE_REGIONS')
+    raw_resource_regions = os.environ.get('COST_REPORT_RESOURCE_REGIONS')
     if raw_resource_regions: 
         raw_resource_regions = raw_resource_regions[1:-1]
         resource_regions.extend([x for x in raw_resource_regions.split(",")])
@@ -31,13 +31,13 @@ def get_resource_regions():
 
 def lambda_handler(event, context):    
     # Drop and re-create DynamoDB table
-    table = dynamodb.recreate_table(dynamodb_client, COST_REPORT_DDB_TABLE_NAME)
+    # table = dynamodb.recreate_table(dynamodb_client, COST_REPORT_DDB_TABLE_NAME)
 
-    if not table:
-        return {
-        'statusCode': 404,
-        'body': json.dumps('Failed to re-create DynamoDB table!')
-    }
+    # if not table:
+    #     return {
+    #     'statusCode': 404,
+    #     'body': json.dumps('Failed to re-create DynamoDB table!')
+    # }
 
     for region in get_resource_regions():
         config = Config(region_name=region)
